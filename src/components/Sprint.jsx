1656d1f27a7f935681f4b72285e18d985e060fc6
Regourd Colin <colin.regourd@inetum.world>
2021-02-18 16:01:57 +0100

    changement vers appel sprint api

--- src/components/Sprint.jsx
+++ src/components/Sprint.jsx
@@ -6,29 +6,29 @@ import moment                          from 'moment'
 import timezone                        from 'moment-timezone'
 
 
-class Clock extends Component {
+class Sprint extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
         return (
-            <div></div>
+            <div>Hello</div>
         );
     }
 }
 
-Clock.displayName = 'Clock';
+Sprint.displayName = 'Sprint';
 
-Clock.propTypes = {
+/*Clock.propTypes = {
     title:    PropTypes.string,
     info:     PropTypes.string,
     timezone: PropTypes.string,
     sunRise:  PropTypes.string.isRequired,
     sunSet:   PropTypes.string.isRequired
-};
+};*/
 
 
 
 
-export default Clock;
+export default Sprint;
