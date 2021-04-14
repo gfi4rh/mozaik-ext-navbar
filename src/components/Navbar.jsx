07b6f65ba3df3b4ea43ec73d6e3eebde25ef740a
Regourd Colin <colin.regourd@inetum.world>
2021-04-14 14:10:48 +0200

    end css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,14 +40,12 @@ class Navbar extends Component {
 
         return (
             <div className="navbar__container">
-                <div>
-                    <Pause className="navbar__playstop"/>
-                    <Forward className="navbar__forward"/>
-                </div>
+                <div className="navbar__svg"><Pause/></div>
+                <div className="navbar__svg"><Forward/></div>
                 <label className="navbar__msg">
                     Le message est ici
                 </label>
-                <div><Login className="navbar__log"/></div>
+                <div className="navbar__svg"><Login/></div>
                 <div className="navbar__version">v 1.0.0</div>
             </div>
         );
