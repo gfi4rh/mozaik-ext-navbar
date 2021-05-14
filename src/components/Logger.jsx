c363cda8bde5c9e2f69b82f8de1d2bd481400ebc
Regourd Colin <colin.regourd@inetum.world>
2021-05-14 09:47:22 +0200

    change form logger

--- src/components/Logger.jsx
+++ src/components/Logger.jsx
@@ -4,24 +4,40 @@ class Logger extends Component {
 
   constructor(props){
     super(props);
+    this.state = {
+      username : '',
+      password : '',
+    }
+    this.handleChange = this.handleChange.bind(this)
   }
+  
+  handleChange(e){
+    const name = e.target.name
+    this.setState({
+      [name] : e.target.value
+    })
+  }
+
 
   render(){
 
     const { handleClose } = this.props
+    const { username, password } = this.state
 
     return(
-      <div>
-        <form method="POST" action="/login" className="logger__form">
+      <div  className="logger__form">
+        <div className="logger__field">
           <label htmlFor="username" className="logger__label">Username</label>
-          <input type="text" name="username" className="logger__input"/>
+          <input type="text" name="username" value={username} className="logger__input" onChange={this.handleChange}/>
+        </div>
+        <div className="logger__field">
           <label htmlFor="password" className="logger__label">Password</label>
-          <input type="text" name="password" className="logger__input"/>
-          <div className="logger__button__container">
-            <button type="button" className="logger__button" onClick={handleClose}>Annuler</button>
-            <button type="submit" className="logger__button">Connexion</button>
-          </div>
-        </form>
+          <input type="password" name="password" value={password} className="logger__input" onChange={this.handleChange}/>
+        </div>
+        <div className="logger__button__container">
+          <button type="button" className="logger__button" onClick={handleClose}>Annuler</button>
+          <button type="submit" className="logger__button">Connexion</button>
+        </div>
       </div>
     );
   }
