010c7674fc95b45f670f24b87e0014e8834aee66
Regourd Colin <colin.regourd@inetum.world>
2021-05-14 10:44:20 +0200

    set regex to match username and password

--- src/components/Logger.jsx
+++ src/components/Logger.jsx
@@ -6,33 +6,43 @@ class Logger extends Component {
     super(props);
     this.state = {
       username : '',
+      username_status : true,
       password : '',
+      password_status : true
     }
     this.handleChange = this.handleChange.bind(this)
   }
   
   handleChange(e){
     const name = e.target.name
+    const value = e.target.value
+    const regex = /^[a-zA-Z0-9!@#$&()\\-`.+,\"]*$/gm
+
     this.setState({
       [name] : e.target.value
     })
+
+    this.setState({
+      [name+'_status'] : regex.exec(value) != null
+    })
+
   }
 
 
   render(){
 
     const { handleClose } = this.props
-    const { username, password } = this.state
+    const { username, password, username_status, password_status } = this.state
 
     return(
       <div  className="logger__form">
         <div className="logger__field">
           <label htmlFor="username" className="logger__label">Username</label>
-          <input type="text" name="username" value={username} className="logger__input" onChange={this.handleChange}/>
+          <input type="text" name="username" style={{"border-color" : username_status ? 'green' : 'red'}} value={username} className="logger__input" onChange={this.handleChange}/>
         </div>
         <div className="logger__field">
           <label htmlFor="password" className="logger__label">Password</label>
-          <input type="password" name="password" value={password} className="logger__input" onChange={this.handleChange}/>
+          <input type="password" name="password" style={{"border-color" : password_status ? 'green' : 'red'}} value={password} className="logger__input" onChange={this.handleChange}/>
         </div>
         <div className="logger__button__container">
           <button type="button" className="logger__button" onClick={handleClose}>Annuler</button>
