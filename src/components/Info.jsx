d54fd0320198fe01e356f2faf304d99ce9ccd2d6
Regourd Colin <colin.regourd@inetum.world>
2021-06-03 12:38:58 +0200

    structure of info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,10 +8,19 @@ class Info extends Component {
 
   render(){
 
-    const { version } = this.props
+    const { info } = this.props
 
     return(
-      <div className="info__container">{version}</div>
+      <div className="info__container">
+        <p>Version : <b>{info.version}</b></p>
+        <hr/>
+        <p>Dernier commit : <b>{info.last_commit}</b></p>
+        <p>Autheur : <b>{info.commit_author}</b></p>
+        <p>Message : <b>{info.commit_message}</b></p>
+        <hr/>
+        <p>Build timestamp : <b>{info.last_build}</b></p>
+        <p></p>
+    </div>
     )
   }
 
